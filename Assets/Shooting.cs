fbcc84e9c8b9f50e8887aa79f0326968ebae30ae
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2023-04-23 01:23:46 -0700

    Player is resized, border increased, added stick weapon that rotates with mouse and fires projectiles to mouse.

---
+++ Assets/Shooting.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
+
+    //Shooting.cs is slightly modified from https://www.youtube.com/watch?v=-bkmPm_Besk
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+        if (!PlayerMovement.flipped)
+        {
+            float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        }
+        else
+        {
+            float rotZ = Mathf.Atan2(-rotation.y, -rotation.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        }
+
+        if(!canFire)
+        {
+            timer += Time.deltaTime;
+            if(timer > timeBetweenFiring)
+            {
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if(Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+
+        }
+    }
+}
--- Assets/playershoot.cs
+++ Assets/playershoot.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sprite : MonoBehaviour
+public class playershoot : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -16,5 +15,4 @@ public class sprite : MonoBehaviour
     {
         
     }
-
 }
